2f917c97fd7391a31c3d4f8357419f5e95c7af35
fodeyen <furkanodeyen97@gmail.com>
2024-04-07 00:43:14 +0300

    added service

--- src/main/java/com/maptracker/service/PointService.java
+++ src/main/java/com/maptracker/service/PointService.java
@@ -1,2 +1,30 @@
-package com.maptracker.service;public class PointService {
+package com.maptracker.service;
+
+
+import com.maptracker.model.Point;
+import com.maptracker.repository.PointRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PointService {
+
+    private final PointRepository pointRepository;
+
+    public PointService(PointRepository pointRepository) {
+        this.pointRepository = pointRepository;
+    }
+
+    public List<Point> getAllPoints() {
+        return pointRepository.findAll();
+    }
+
+    public Point savePoint(Point point) {
+        return pointRepository.save(point);
+    }
+
+    public void deletePoint(Long id) {
+        pointRepository.deleteById(id);
+    }
 }
