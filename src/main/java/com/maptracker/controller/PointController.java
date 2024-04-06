7de85ed92bf72cc356d5825200fb3c268d854d9e
fodeyen <furkanodeyen97@gmail.com>
2024-04-07 00:43:38 +0300

    added controller

--- src/main/java/com/maptracker/controller/PointController.java
+++ src/main/java/com/maptracker/controller/PointController.java
@@ -1,2 +1,37 @@
-package com.maptracker.controller;public class PointController {
+package com.maptracker.controller;
+
+
+import com.maptracker.model.Point;
+import com.maptracker.service.PointService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/points")
+public class PointController {
+
+    private final PointService pointService;
+
+    public PointController(PointService pointService) {
+        this.pointService = pointService;
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("get-all-points")
+    public List<Point> getAllPoints() {
+        return pointService.getAllPoints();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("save-point")
+    public Point savePoint(@RequestBody Point point) {
+        return pointService.savePoint(point);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @DeleteMapping("/delete/{id}")
+    public void deletePoint(@PathVariable Long id) {
+        pointService.deletePoint(id);
+    }
 }
