dc3ebddbf402a53e5d46644520d53edbfc4136fb
fodeyen <furkanodeyen97@gmail.com>
2024-04-07 00:44:52 +0300

    added mysql database

---
+++ src/main/java/com/maptracker/MaptrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaptrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaptrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maptracker/MaptrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaptrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
