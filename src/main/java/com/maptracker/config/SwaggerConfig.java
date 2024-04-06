a28ff4186ef7c2b5492b030b95eaf749f84d6d32
fodeyen <furkanodeyen97@gmail.com>
2024-04-07 00:44:23 +0300

    added swagger

--- src/main/java/com/maptracker/config/SwaggerConfig.java
+++ src/main/java/com/maptracker/config/SwaggerConfig.java
@@ -1,2 +1,32 @@
-package com.maptracker.config;public class SwaggerConfig {
+package com.maptracker.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.maptracker.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Leaflet Project API")
+                .description("Leaflet Project API Documentation")
+                .version("1.0")
+                .build();
+    }
 }
